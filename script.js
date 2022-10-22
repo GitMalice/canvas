cb5df9d9abe13d86537da3537350fa019a49d179
Malice <create@hagalaz.fr>
2022-10-22 13:07:40 +0200

    tracé des cercles correct

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const radius = 196 / 2
 
 if (canvas.getContext) {
   const ctx = canvas.getContext('2d')
-  ctx.beginPath()
-  ctx.arc(radius, radius, radius, 0, Math.PI * 2)
-  const seven = radius / 7
-  for (let i = 1; i < 6; i++) {
-    ctx.arc(radius, radius, seven * i, Math.PI * 2)
+  // ctx.arc(radius, radius, radius, 0, Math.PI * 2)
+  const seven = Math.round(radius / 7)
+  for (let i = 1; i < 8; i++) {
+    ctx.beginPath()
+    ctx.arc(radius, radius, seven * i, 0, Math.PI * 2)
+    ctx.stroke()
   }
-  ctx.stroke()
 }
